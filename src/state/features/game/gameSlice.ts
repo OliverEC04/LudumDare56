e902091467d702174aefcfb9544af3278566b963
OliverEC <oliverec04@gmail.com>
2024-10-05 13:43:16 +0200

    create game slice

---
+++ src/state/features/game/gameSlice.ts
@@ -0,0 +1,29 @@
+﻿import {createSlice} from '@reduxjs/toolkit'
+import {Node} from "../../../models/node.ts";
+import {Tunnel} from "../../../models/tunnel.ts";
+
+interface GameState {
+    nodes: Node[];
+    tunnels: Tunnel[];
+    termites: number;
+}
+
+const initialState: GameState = {
+    nodes: [],
+    tunnels: [],
+    termites: 10
+}
+
+export const gameSlice = createSlice({
+    name: 'game',
+    initialState,
+    reducers: {
+        update: (state) => {
+            state.termites = state.termites * 1.1;
+        }
+    },
+})
+
+export const {update} = gameSlice.actions;
+
+export default gameSlice.reducer;
