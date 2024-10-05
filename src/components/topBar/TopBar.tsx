eda3b2fa049a81969ef035ea3c0f749d7242588f
OliverEC <oliverec04@gmail.com>
2024-10-05 20:44:52 +0200

    termites top bar

--- src/components/topBar/TopBar.tsx
+++ src/components/topBar/TopBar.tsx
@@ -1,10 +1,13 @@
 ﻿import {Paper, SxProps} from '@mui/material';
 import {TopBarTile} from './TopBarTile.tsx';
+import {useAppSelector} from '../../state/hooks.ts';
 
 export const TopBar = () => {
+	const {termites} = useAppSelector((state) => state.game);
+
 	return (
 		<Paper sx={styles.container}>
-			<TopBarTile label="Termites" value={10}/>
+			<TopBarTile label="Termites" value={termites}/>
 		</Paper>
 	);
 };
