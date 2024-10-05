93a1aca78e0b3500cab64f1e763ced297b8a9c77
OliverEC <oliverec04@gmail.com>
2024-10-05 13:18:04 +0200

    added classes node and tunnel

---
+++ src/models/node.ts
@@ -0,0 +1,31 @@
+﻿import {Tunnel} from "./tunnel.ts";
+
+export enum NodeType {
+    none,
+    food,
+    mud,
+    feces,
+    home
+}
+
+export class Node {
+    x: number;
+    y: number;
+    type: NodeType;
+    size: number;
+    tunnels: Tunnel[];
+
+    constructor(
+        x: number,
+        y: number,
+        type: NodeType,
+        size: number,
+        tunnels: Tunnel[]
+    ) {
+        this.x = x;
+        this.y = y;
+        this.type = type;
+        this.size = size;
+        this.tunnels = tunnels;
+    }
+}
---
+++ src/models/tunnel.ts
@@ -0,0 +1,32 @@
+﻿import {Node} from "./node.ts";
+
+export enum TunnelType {
+    dug,
+    mud,
+    feces
+}
+
+export class Tunnel {
+    begin: Node;
+    end: Node;
+    damage: number[];
+    size: number;
+    type: TunnelType;
+    enabled: boolean;
+
+    constructor(
+        begin: Node,
+        end: Node,
+        damage: number[],
+        size: number,
+        type: TunnelType,
+        enabled: boolean
+    ) {
+        this.begin = begin;
+        this.end = end;
+        this.damage = damage;
+        this.size = size;
+        this.type = type;
+        this.enabled = enabled;
+    }
+}
