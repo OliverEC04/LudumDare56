8a936b33a722df98b0cb5f18eba1362e9342bd01
OliverEC <oliverec04@gmail.com>
2024-10-05 23:59:02 +0200

    tools work

--- src/components/topBar/TopBarTile.tsx
+++ src/components/topBar/TopBarTile.tsx
@@ -1,11 +1,16 @@
 ﻿import {Box} from '@mui/material';
 
 interface Props<T> {
+	icon?: string;
+	text?: string;
 	label: string;
 	value: T;
 }
 
-export const TopBarTile = <T, >({label, value}: Props<T>) => {
+export const TopBarTile = <T, >({icon, text, label, value}: Props<T>) => {
+
+	// const iconComp = (<Box></Box>);
+
 	return (
 		<Box>{label}: {value as string}</Box>
 	);
